875ddbf07890a082e67469e7c9a34997ec04c796
BioIst <71051549+YERIM-HEO@users.noreply.github.com>
2021-06-02 21:22:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const toggle = document.querySelector('.navbar__tool');
+const toggleBtn = document.querySelector('.navbar_tool');
 const menu = document.querySelector('.navbar_menu');
 
-toggle.addEventListener('click', ( ) => {
+toggleBtn.addEventListener('click', ( ) => {
      menu.classList.toggle('active');
 });
 
 const menu_home = document.querySelector('#menu_home');
 const menu_major = document.querySelector('#menu_major');
 const menu_history = document.querySelector('#menu_history');
-
+const main_title = document.querySelector("#main_title")
 
 menu_home.addEventListener('click', ( ) => {
     document.querySelector("#menu_home").style.color = "black";
@@ -18,6 +18,7 @@ menu_home.addEventListener('click', ( ) => {
     document.querySelector("#major").style.display = "none";
     document.querySelector("#history").style.display = "none";
 });
+
 
 menu_major.addEventListener('click', ( ) => {
     document.querySelector("#menu_home").style.color = "grey";
@@ -35,4 +36,13 @@ menu_history.addEventListener('click', ( ) => {
     document.querySelector("#home").style.display = "none";
     document.querySelector("#major").style.display = "none";
     document.querySelector("#history").style.display = "block";
+});
+
+main_title.addEventListener('click', ( ) => {
+    document.querySelector("#menu_home").style.color = "black";
+    document.querySelector("#menu_major").style.color = "grey";
+    document.querySelector("#menu_history").style.color = "grey";
+    document.querySelector("#home").style.display = "block";
+    document.querySelector("#major").style.display = "none";
+    document.querySelector("#history").style.display = "none";
 });
